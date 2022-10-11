9933c85ea5601cd992ecf58fd8756ab8166a0e47
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-10-11 21:38:14 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  const menuOpen = document.querySelector(".hamburger"),
+        menuClose = document.querySelector(".header__menu-close"),
+        menu = document.querySelector(".header__menu"),
+        burgerStickTop = menuOpen.querySelector(".hamburger__stick-top"),
+        burgerStickMiddle = menuOpen.querySelector(".hamburger__stick-middle"),
+        burgerStickBottom = menuOpen.querySelector(".hamburger__stick-bottom");
+
+  menuOpen.addEventListener("click", (event) => {
+    setTimeout(() => {
+      menu.classList.toggle("header__menu_active");
+    }, 100);
+    burgerActive();
+  });
+
+  menuClose.addEventListener("click", (event) => {
+    setTimeout(() => {
+      menu.classList.toggle("header__menu_active");
+    }, 200);
+    menuClose.classList.add("header__menu-close_animation");
+    setTimeout(burgerActive, 400);
+    setTimeout(() => {
+      menuClose.classList.remove("header__menu-close_animation");
+    }, 600);
+    
+  });
+
+  function burgerActive() {
+    burgerStickTop.classList.toggle("hamburger__stick-top_active");
+    burgerStickMiddle.classList.toggle("hamburger__stick-middle_active");
+    burgerStickBottom.classList.toggle("hamburger__stick-bottom_active");
+  }
+
+
+
+
+});
