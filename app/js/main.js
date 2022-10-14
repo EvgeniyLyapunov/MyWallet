1f9beb3793f25b2d748a30e2b93164cb8c3e091e
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-10-14 10:14:25 +0300

    work over balance modal

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
+  // burger-menu
   const menuOpen = document.querySelector(".hamburger"),
         menuClose = document.querySelector(".header__menu-close"),
         menu = document.querySelector(".header__menu"),
@@ -34,17 +35,18 @@ window.addEventListener("DOMContentLoaded", () => {
     burgerStickBottom.classList.toggle("hamburger__stick-bottom_active");
   }
 
-  const btns = document.querySelectorAll(".btn");
-  btns.forEach((btn) => {
-    btn.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      pressBtn(btn);
-    });
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      pressBtn(btn);
-    });
-  });
+  // buttons presses
+  // const btns = document.querySelectorAll(".btn");
+  // btns.forEach((btn) => {
+  //   btn.addEventListener("touchstart", (e) => {
+  //     e.preventDefault();
+  //     pressBtn(btn);
+  //   });
+  //   btn.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     pressBtn(btn);
+  //   });
+  // });
   
   function pressBtn(button) {
     button.classList.add("btn_active");
@@ -61,5 +63,21 @@ window.addEventListener("DOMContentLoaded", () => {
   // const modalHeight = body.offsetHeight - (header.clientHeight + footer.clientHeight);
   // console.log(modalHeight);
   // console.log(document.querySelector("main").offsetHeight);
+
+  // modals
+  const viewBalanceBtn = document.querySelector("#viewBalance"),
+        mainSection = document.querySelector("main"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector(".modal__close");
+
+  viewBalanceBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    pressBtn(event.target);
+    setTimeout(() => {
+      modal.classList.add("visible");
+      mainSection.classList.add("hide");
+    }, 300);
+    
+  });
 
 });
