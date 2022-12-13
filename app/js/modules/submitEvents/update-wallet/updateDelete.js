98fcaf726b1f4afd72f7990ca688a2a3cc0c9d46
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-12-13 20:16:28 +0300

    added info about virtuals wallet when base wallet delete

--- app/js/modules/submitEvents/update-wallet/updateDelete.js
+++ app/js/modules/submitEvents/update-wallet/updateDelete.js
@@ -8,24 +8,41 @@ import { openViewBalansWindow } from '../../viewBalance';
 import getDataFromStorage from '../../../services/getDataFromStorage';
 
 let currentCard = {};
+let data = [];
+let virtCard = [];
+const infoAboutVirtWallets = document.querySelector('.delete-modal__warning-infoAboutVirts');
 
 function changeDelete(card) {
+
+  // очищаем общие переменные и сообщения с возможного прошлого события
+  currentCard = {};
+  data = [];
+  virtCard = [];
+  infoAboutVirtWallets.textContent = '';
+
   currentCard = card;
+
+  data = getDataFromStorage();
+  virtCard = data.filter(item => {
+    if(item.baseStorageId === currentCard.id) {
+      return item.id;
+    }
+  }).map(item => item.id);
+
   const currentName = document.querySelector('#delete-wallet-current-name');
   currentName.textContent = `${currentCard.name}`;
+
+  if(virtCard.length > 0) {
+    let infoText = `У кошелька ${currentCard.name} есть`;
+    infoText += (virtCard.length > 1) ? ` виртуальные кошельки.\n Они тоже будут удалены.` : ` виртуальный кошелёк. Он тоже будет удалён.`;
+    infoAboutVirtWallets.textContent = infoText;
+  }
 }
 
 async function deleteSubmit(e) {
   pressBtn(e.target);
 
   let walletDelete = {id: currentCard.id, userId: currentCard.userId};
-
-  const data = getDataFromStorage();
-  const virtCard = data.filter(item => {
-    if(item.baseStorageId === walletDelete.id) {
-      return item.id;
-    }
-  }).map(item => item.id);
 
   walletDelete.virts = virtCard;
 
@@ -43,6 +60,9 @@ async function deleteSubmit(e) {
     });
 
     currentCard = {};
+    data = [];
+    virtCard = [];
+
     if(localStorage.getItem("balanceData")) {
       localStorage.setItem("balanceData", `${JSON.stringify(answer.data)}`);
     } else  {
@@ -64,6 +84,8 @@ async function deleteSubmit(e) {
 function cancelSubmit(e) {
   pressBtn(e.target);
   currentCard = {};
+  data = [];
+  virtCard = [];
   openViewBalansWindow();
 }
 
